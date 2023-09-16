3495365641a7e5d8287f648d455da9b83347b86c
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2023-09-16 23:46:47 +0300

    update

--- controle-de-candidatos/src/candidatura/processoSeletivo.java
+++ controle-de-candidatos/src/candidatura/processoSeletivo.java
@@ -19,24 +19,25 @@ public class processoSeletivo {
     }
     
     /*  -------------  METODOS ---------------------- */
-
+    
     /*  ---- metodos para selecionar os candidatos ----- */
-    public static void selecaoDeCandidatos() {
+    public static  void selecaoDeCandidatos() {
         
-            String [] candidatos = {"cand 1","cand 2","cand 3","cand 4","cand 5"};
-            
+        String [] candidatos = {"cand 1","cand 2","cand 3","cand 4","cand 5"};
 
              int candidatosSelecionados = 0;
              int candidatoAtual = 0;
              double salarioBase = 2000.0;
+            /*  String [] selecionados = {}; */
              
-             while (candidatosSelecionados < 3 && candidatoAtual<candidatos.length) {
+             while (candidatosSelecionados <= 3 && candidatoAtual<candidatos.length) {
                  String candidato = candidatos[candidatoAtual];
                  double salarioPretendido = valorPretendido();
                 
                 System.out.println("O candidato "+candidato+" solicitou um salario de R$ "+salarioPretendido+".");
                 if (salarioBase >= salarioPretendido) {
                     System.out.println("E foi  SELECIONADO.");
+                    /* selecionados[candidatoAtual] = candidatos[candidatoAtual]; */
                     candidatosSelecionados++;   
                 } else {
                     System.out.println("E NAO foi  SELECIONADO.");    
@@ -44,6 +45,10 @@ public class processoSeletivo {
                 /*  String aprovados = candidatos[candidatoAtual]; */
                 candidatoAtual++;
             }
+
+            /* for (int i = 0; i < selecionados.length; i++) {
+             System.out.println(selecionados[i]);
+            } */
         }
         /* ---- metodos auxiliares -----  */
         static boolean ligar() {
