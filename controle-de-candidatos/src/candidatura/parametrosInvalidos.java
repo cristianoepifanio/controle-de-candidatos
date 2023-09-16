21d0f009032fdee12cf9adff8f0222940802db8f
Cristiano Epifânio <106474458+cristianoepifanio@users.noreply.github.com>
2023-09-16 13:08:53 +0300

    tentando realizar o desafio

--- controle-de-candidatos/src/candidatura/parametrosInvalidos.java
+++ controle-de-candidatos/src/candidatura/parametrosInvalidos.java
@@ -1,5 +1,13 @@
 package candidatura;
 
-public class parametrosInvalidos {
-    
+public class parametrosInvalidos extends exception{
+    public static void main(String[] args) {
+        try {
+            
+        } catch (java.lang.ArithmeticException e) {
+            // TODO: handle exception
+            System.out.println("o numero 2 precisa ser maior que o numero 1.");
+        }
+    }
+   
 }
